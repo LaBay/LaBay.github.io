6d461e16e343c41358a829c1f15f33016bd47646
“LaBay” <labay@i.ua>
2016-06-23 02:45:44 +0300

    .

--- final/js/_script.js
+++ final/js/_script.js
@@ -60,12 +60,12 @@ $('#step-section__content__item--3--right').on('click', function(){
 
 $(function(){
 
-const quantity = 6;
+const quantity = 7;
 const BEMclass = 'idea-section__content__item';
-var newElements = '';
+var newElemets = '';
 var BEMselector = 1;
 var $queryBlock = $('#queryBlock');
-var fatItem1, fatItem2, fatItem3;
+var fatItem1, fatItem2;
 var queries = ['Sports and Activity', 'Welness and Health', 'Extreme Sports and Expeditions', 'Games', 'Culture and Education', 'Relaxation', 'Travelling'];
 
 
@@ -73,63 +73,87 @@ var queries = ['Sports and Activity', 'Welness and Health', 'Extreme Sports and 
 		
 		$queryBlock.html('');
 
-		while(BEMselector <= quantity){
-				let str = '<li class="' + BEMclass + ' ' + BEMclass + '--' + BEMselector + '">' + '</li> ';
-				BEMselector++;
-				newElements += str;
+		function fatalizator(fat){
+		
+				if (fat < 0.08){
+					fatItem1 = 1;
+					fatItem2 = 3;
+				};
+				if (fat>0.08 && fat<0.17){
+					fatItem1 = 1;
+					fatItem2 = 4;
+				};
+				if (fat>0.17 && fat<0.25){
+					fatItem1 = 1;
+					fatItem2 = 6;
+				};		    
+				if (fat>0.25 && fat<0.33){
+					fatItem1 = 1;
+					fatItem2 = 7;
+				};
+
+				if (fat>0.33 && fat<0.42){
+					fatItem1 = 2;
+					fatItem2 = 3;
+				};
+				if (fat>0.42 && fat<0.5){
+					fatItem1 = 2;
+					fatItem2 = 4;
+				};
+				if (fat>0.5 && fat<0.58){
+					fatItem1 = 2;
+					fatItem2 = 6;
+				};			    
+				if (fat>0.58 && fat<0.67){
+					fatItem1 = 2;
+					fatItem2 = 7;
+				};
+
+				if (fat>0.67 && fat<0.75){
+					fatItem1 = 4;
+					fatItem2 = 6;
+				};
+				if (fat>0.75 && fat<0.83){
+					fatItem1 = 4;
+					fatItem2 = 7;
+				};
+				if (fat>0.83 && fat<0.92){
+					fatItem1 = 5;
+					fatItem2 = 6;
+				}		    
+				if (fat > 0.92){
+					fatItem1 = 5;
+					fatItem2 = 7;
+				};
+
+		}fatalizator(Math.random());
+
+
+		function fatInsert(){
+			if ((BEMselector == fatItem1) || (BEMselector == fatItem2))	return ' fat';
+			else return '';
 		};
 
-		$queryBlock.html(newElements);
+		while(BEMselector <= quantity){
+				let str = '<li class="' + BEMclass + ' ' + BEMclass + '--' 
+				+ BEMselector + fatInsert() + '">' + '</li> ';
+				BEMselector++;
+				newElemets += str;
+		};
+
+	$queryBlock.html(newElemets);
+
 
 	}create();
 
 	var $block = $('.' + BEMclass);	
 
-	function fatalizator(){
-		let i = Math.random();
-		let k = Math.random();
-		let j = Math.random();
-
-			if (i < 0.6){
-				fatItem1 = 0;
-			}else{
-				fatItem1 = 1;
-			};
-
-			if (k < 0.5){
-				fatItem2 = 2;
-			}else{
-				fatItem2 = 3;
-			};
-			
-			if (j < 0.4){
-				fatItem3 = 4;
-			}else{
-				fatItem3 = 5;
-			};
-
-			for (let i = 0; i < $block.length; i++) $block.eq(i).removeClass('fat');
-			
-			$block.eq(fatItem1).addClass('fat');
-			$block.eq(fatItem2).addClass('fat');
-			$block.eq(fatItem3).addClass('fat');
-			
-	}fatalizator();
-
-
 	function getImage(el, i){
 
-		$.getJSON('https://pixabay.com/api/?key=2799655-a46e55bb99fc16063276b4cfb&q=' + el + '&image_type=photo', function(r){
-				//console.log(r)
-				if (!(r.hits.length)) {
-					$block.eq(i).css('background-image', 'url(' + 'background/idea_bg_' + i + '.jpg' + ')')
-							.html('<span>NO RESULTS</span>')
-							return;
-				} ;
-				let j = (r.hits.length * Math.random()) >> 0;
-				//console.log('r.hits.length=', r.hits.length,'j=', j);
-				$block.eq(i).css('background-image', 'url(' +r.hits[j].webformatURL + ')')
-							.html('<span>' + r.hits[j].tags + '</span>')
+		$.getJSON('http://api.pixplorer.co.uk/image?word=' + el + '&amount=1&?size=l(large)', function(r){
+				console.log(r)
+				$block.eq(i).css('background-image', 'url(' +r.images[0].imageurl+ ')')
+							.html('<span>' + r.images[0].word + '</span>')
 		});
 	};
 
@@ -154,7 +178,6 @@ var queries = ['Sports and Activity', 'Welness and Health', 'Extreme Sports and 
 
 	$('#interests-search_btn').on('click', function(){
 		let value = $('#interests-search_field').val();
-		fatalizator();
 		search(value);
 	});
 
